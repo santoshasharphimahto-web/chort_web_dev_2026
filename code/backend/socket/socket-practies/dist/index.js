8e5444252c8c6c5658216718f0f6850e67bd49b1
santoshasharphimahto-web <santoshasharphimahto@gmail.com>
2026-08-22 16:10:46 +0530

    socke-implementing

---
+++ code/backend/socket/socket-practies/dist/index.js
@@ -0,0 +1,21 @@
+import http from "node:http";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+function main() {
+    const app = express();
+    const server = http.createServer(app);
+    const port = process.env.PORT || 3000;
+    const io = new Server(server);
+    // const publicDirectory = path.resolve(__dirname, "../public");
+    app.use(express.static(path.resolve('./public')));
+    server.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+main();
+//# sourceMappingURL=index.js.map
